c37be1e54c2e26cce7e487a0871097374132bb5d
czhang261 <czhang261@CZHANG26155.us.nam.ad.pwcinternal.com>
2014-08-13 13:46:55 +0800

    add log info into Servlet and add log4j configuration

--- ShowMessage/src/main/java/com/pwc/servlet/DataCombatServlet.java
+++ ShowMessage/src/main/java/com/pwc/servlet/DataCombatServlet.java
@@ -10,11 +10,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Servlet implementation class DataCombatServlet
  */
 @WebServlet("/DataCombatServlet")
 public class DataCombatServlet extends HttpServlet {
+	private static final Log log = LogFactory.getLog(DataCombatServlet.class);
 	private Map<String,String> eTagMap = new HashMap<String,String>();
 	//sessionId used for session record
     //private String sessionId = null;  
@@ -35,17 +39,21 @@ public class DataCombatServlet extends HttpServlet {
 		if (uri.endsWith("jsp")) {
 			//if the session is valid,retrieve the data as per the seesionId
 			String sessionId = (String)request.getSession().getAttribute("Etag");
+			log.info("current Etag is: " + sessionId);
 			if(null != sessionId || "".equals(sessionId)){
 				String newString = eTagMap.get(sessionId);
+				log.info("Etag : " + sessionId + "contains value " + newString);
 				request.setAttribute("newString", newString);
 			}
 			uri = "showdata.jsp";
 		} else if (uri.endsWith("jpg")) {
 			//check whether If-None_Match header info exists or not
 			String etag = request.getHeader("If-None-Match");
+			log.info("retrieve Etag: " + etag);
 			if (etag != null && !"".equals(etag)) {
 				//System.out.println(etag);
 				String newString = eTagMap.get(etag);
+				log.info("Etag : " + etag + "contains value " + newString);
 				if(newString != null){
 					//System.out.println("get value from client: " + newString);
 					request.setAttribute("newString", newString);
@@ -62,6 +70,8 @@ public class DataCombatServlet extends HttpServlet {
 		request.getRequestDispatcher(uri).forward(request, response);
 		//if the current sessionId is null,retrieve Etag from the response
 		request.getSession().setAttribute("Etag", response.getHeader("Etag"));
+		if(response.getHeader("Etag") != null)
+		log.info("Set Etag: " + response.getHeader("Etag") + " into Session");
 //		if(null == sessionId){
 //			//sessionId = response.getHeader("Etag");
 //			
@@ -79,6 +89,7 @@ public class DataCombatServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		String new_string = request.getParameter("newString");
 		String etag = (String)request.getSession().getAttribute("Etag");
+		log.info("Retrieve data: " + new_string + "and Etag: " + etag);
 		//retrieve sessionId from previous GET 
 		eTagMap.put(etag,new_string);
 		//request.getSession().setAttribute("newSting", new_string);
